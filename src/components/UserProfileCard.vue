e767f7ebf339ee18bae28d4285aca9b9fd5b32f0
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2020-07-29 19:41:12 +0200

    Toggle Profile show/edit

--- src/components/UserProfileCard.vue
+++ src/components/UserProfileCard.vue
@@ -34,9 +34,13 @@
 
     <div class="text-center">
       <hr>
-      <a href="edit-profile.html" class="btn-green btn-small">Edit Profile</a>
+      <router-link
+        :to="{name: 'ProfileEdit'}"
+        class="btn-green btn-small"
+      >
+        Edit Profile
+      </router-link>
     </div>
-
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,14 @@ export default new Router({
     {
       path: '/me',
       name: 'Profile',
-      component: Profile
+      component: Profile,
+      props: true
+    },
+    {
+      path: '/me/edit',
+      name: 'ProfileEdit',
+      component: Profile,
+      props: {edit: true}
     },
     {
       path: '*',
