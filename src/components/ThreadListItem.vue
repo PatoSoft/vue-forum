86c68b4ec87fd1cb96ef13862c3e0ad9561f1305
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2020-07-29 19:29:49 +0200

    Update profile

--- src/components/ThreadListItem.vue
+++ src/components/ThreadListItem.vue
@@ -30,6 +30,8 @@
 </template>
 
 <script>
+  import {countObjectProperties} from '@/utils'
+
   export default {
     props: {
       thread: {
@@ -40,7 +42,7 @@
 
     computed: {
       repliesCount () {
-        return Object.keys(this.thread.posts).length - 1
+        return countObjectProperties(this.thread.posts) - 1
       },
       user () {
         return this.$store.state.users[this.thread.userId]
