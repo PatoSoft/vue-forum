b51b2c7e28097340ba7dd7898591242a403d4bfa
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2020-07-29 19:52:02 +0200

    Small fixes

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -36,13 +36,11 @@
       }
     },
 
-    data () {
-      return {
-        thread: this.$store.state.threads[this.id]
-      }
-    },
-
     computed: {
+      thread () {
+        return this.$store.state.threads[this.id]
+      },
+
       posts () {
         const postIds = Object.values(this.thread.posts)
         return Object.values(this.$store.state.posts)
