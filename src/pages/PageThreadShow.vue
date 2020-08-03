d4110baeaa9c385639757f3fc0b5be6f2fb6aad5
Jose Luis Martinez Ortiz <maorjl@gmail.com>
2020-08-03 18:36:51 +0200

    Fetch data from Firebase with Vuex Actions

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -22,7 +22,6 @@
 </template>
 
 <script>
-  import firebase from 'firebase'
   import PostList from '@/components/PostList'
   import PostEditor from '@/components/PostEditor'
   import {countObjectProperties} from '@/utils'
@@ -66,35 +65,15 @@
 
     created () {
       // Fetch thread
-      firebase.database().ref('threads').child(this.id).once('value', snapshot => {
-        const thread = snapshot.val()
-        this.$store.commit('setThread',
-          {threadId: snapshot.key, thread: {...thread, '.key': snapshot.key}}
-        )
-
+      this.$store.dispatch('fetchThread', {id: this.id}).then(thread => {
         // Fetch user
-        firebase.database().ref('users').child(thread.userId).once('value', snapshot => {
-          const user = snapshot.val()
-          this.$store.commit('setUser',
-            {userId: snapshot.key, user: {...user, '.key': snapshot.key}}
-          )
-        })
+        this.$store.dispatch('fetchUser', {id: thread.userId})
 
         Object.keys(thread.posts).forEach(postId => {
           // Fetch post
-          firebase.database().ref('posts').child(postId).once('value', snapshot => {
-            const post = snapshot.val()
-            this.$store.commit('setPost',
-              {postId: snapshot.key, post: {...post, '.key': snapshot.key}}
-            )
-
+          this.$store.dispatch('fetchThread', {id: postId}).then(post => {
             // Fetch user
-            firebase.database().ref('users').child(post.userId).once('value', snapshot => {
-              const user = snapshot.val()
-              this.$store.commit('setUser',
-                {userId: snapshot.key, user: {...user, '.key': snapshot.key}}
-              )
-            })
+            this.$store.dispatch('fetchUser', {id: post.userId})
           })
         })
       })
